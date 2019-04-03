725e98222d09c9ea3fd2155119e94d51fa30dabc
Mothupi Keenan <iamkeenan94@gmail.com>
2019-04-04 01:43:52 +0200

    Initial commit

---
+++ app/src/main/java/com/sirmothupii/codefest2019contex/LandingPageFNB.java
@@ -0,0 +1,13 @@
+package com.sirmothupii.codefest2019contex;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LandingPageFNB extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
