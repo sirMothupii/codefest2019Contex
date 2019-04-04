624ad4f85f96f58d548bf2b96b64a5ba1441ff71
Mareka Kwena <mareka.kwena@fnb.co.za>
2019-04-04 04:05:17 +0200

    Sorry page ui

---
+++ app/src/main/java/com/sirmothupii/codefest2019contex/SorryPage.java
@@ -0,0 +1,13 @@
+package com.sirmothupii.codefest2019contex;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SorryPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sorry_page);
+    }
+}
