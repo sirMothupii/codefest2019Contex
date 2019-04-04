de643068bfaec03fefccf37399726bbf0910a785
Mothupi Keenan <iamkeenan94@gmail.com>
2019-04-04 04:39:24 +0200

    UIDesign

---
+++ app/src/main/java/com/sirmothupii/fnb_codefest2019contex/UserProfile.java
@@ -0,0 +1,15 @@
+package com.sirmothupii.fnb_codefest2019contex;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserProfile extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+    }
+}
